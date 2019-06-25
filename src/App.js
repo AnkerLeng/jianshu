1e2e8dfa803add4e1333cda2e78f6c300e6629bf
AnkerLeng <553628212@qq.com>
2019-06-26 01:57:31 +0800

    8-2 首页组件的拆分

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom'; 
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header/index'
+import Home from './pages/home/index'
+import Detail from './pages/detail/index'
 import store from './store'
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <div>
-        <Header/>
+        <div>
+          <Header />
           <BrowserRouter>
-          <div>
-            <Route path='/' exact render={()=><div>home</div>}></Route>
-            <Route path='/detail' exact render={()=><div>detail</div>}></Route>
-          </div>
+            <div>
+              <Route path='/' exact component={Home}></Route>
+              <Route path='/detail' exact component={Detail}></Route>
+            </div>
           </BrowserRouter>
-      </div>    
+        </div>
       </Provider>
-      
+
     );
   }
 }
---
+++ src/pages/home/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Topic from './components/Topic';
+import List from './components/List';
+import Recomment from './components/Recomment';
+import Writer from './components/Writer';
+import { HomeWrapper, HomeLeft, HomeRight } from './style'
+
+class Home extends Component {
+    render() {
+        return (
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className="banner-img" src="https://upload.jianshu.io/admin_banners/web_images/4660/224da83c76e01d5deff07e163615921233af5c82.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"></img>
+                    <Topic/>
+                    <List/>
+                </HomeLeft>
+                <HomeRight>
+                    <Recomment/>
+                    <Writer/>
+                </HomeRight>
+            </HomeWrapper>
+
+        )
+    }
+}
+
+export default Home;
