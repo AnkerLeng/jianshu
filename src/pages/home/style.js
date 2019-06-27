6b4aa4c638bd7c2a16f50424b6c1eb2e264181c4
AnkerLeng <553628212@qq.com>
2019-06-27 23:47:50 +0800

    8-6 首页异步数据获取 8-7 异步操作代码拆分优化

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,26 +1,25 @@
 import styled from 'styled-components';
 
 export const HomeWrapper = styled.div`
-    overflow: hidden;
-    width: 960px;
-    margin: 0 auto;
+	overflow: hidden;
+	width: 960px;
+	margin: 0 auto;
 `;
 
 export const HomeLeft = styled.div`
-    float: left;
-    margin-left: 15px;
-    padding-top: 30px;
-    width: 625px;
-    .banner-img{
-        width: 625px;
-        height: 270rpx;
-        border-radius: 10px;
-    }
+	float: left;
+	margin-left: 15px;
+	padding-top: 30px;
+	width: 625px;
+	.banner-img {
+		width: 625px;
+		height: 270px;
+	}
 `;
 
 export const HomeRight = styled.div`
-    width: 280px;
-    float: right;
+	width: 280px;
+	float: right;
 `;
 
 export const TopicWrapper = styled.div`
@@ -64,7 +63,7 @@ export const ListItem = styled.div`
 	}
 `;
 
-export const ListInfo = styled.div`
+export const ListInfo =	styled.div`
 	width: 500px;
 	float: left;
 	.title {
@@ -100,3 +99,27 @@ export const WriterWrapper = styled.div`
 	line-height: 300px;
 	text-align: center;
 `;
+
+export const LoadMore = styled.div`
+	width: 100%;
+	height: 40px;
+	line-height: 40px;
+	margin: 30px 0;
+	background: #a5a5a5;
+	text-align:center;
+	border-radius: 20px;
+	color: #fff;
+	cursor: pointer;
+`;
+
+export const BackTop = styled.div`
+	position: fixed;
+	right: 100px;
+	bottom: 100px;
+	width: 60px;
+	height: 60px;
+	line-height: 60px;
+	text-align: center;
+	border: 1px solid #ccc;
+	font-size: 14px;
+`
